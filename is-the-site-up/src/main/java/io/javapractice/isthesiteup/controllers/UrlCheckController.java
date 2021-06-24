4142c5f9aa9a76c591e840a50aa53b577aaed591
ThinushikaDahanayake <thinushidahanayake@gmail.com>
2021-06-24 09:30:51 +0530

    init

---
+++ is-the-site-up/src/main/java/io/javapractice/isthesiteup/controllers/UrlCheckController.java
@@ -0,0 +1,41 @@
+package io.javapractice.isthesiteup.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// Import rest controller
+@RestController
+public class UrlCheckController {
+    private final String SITE_IS_UP = "site is up!";
+    private final String SITE_IS_DOWN = "site is down!";
+    private final String INCORRECT_URL = "URL is incorrect!";
+
+
+    @GetMapping("/check")
+    public String getUrlStatesMessage(@RequestParam String url){
+        String returnMessage = "";
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCodeCategory = conn.getResponseCode() / 100;
+            if (responseCodeCategory != 2 || responseCodeCategory!=3){
+                returnMessage = SITE_IS_DOWN;
+            } else {
+                returnMessage = SITE_IS_UP;
+            }
+        } catch (MalformedURLException e) {
+            returnMessage = INCORRECT_URL;
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+        return returnMessage;
+    }
+}
